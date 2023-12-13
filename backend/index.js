4847cf8c28f49a8b2c9cd069d425bd1c153909dd
Syed Abdul Quadeer Kashif <113990051+Syed-kashif0627@users.noreply.github.com>
2023-12-13 21:38:58 +0530

    salting and hashing user password

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,13 @@ const express = require('express')
 
 connectToMongo();
 const app = express()
-const port = 3000
-
+const port = 5000
+//app.use(express.static(path.join(__dirname,'folderName')))//app.use() is a function that allows us to use Middleware express.static is inbuilt middle ware
+//Custom middleware
+//const kashMiddleware=(req,res,next)=>{
+//   console.log(req)
+//   next()
+// }
 app.use(express.json());
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,26 +1,46 @@
 const express=require('express');
 const User = require('../models/User');
 const {body, validationResult}=require('express-validator');
+const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
 
 const router=express.Router();
 
-router.post('/',[
+router.post('/createUser',[//checking constraints of email ,name ,password....etc;
     body('name','minimum length for name should be 3').isLength({min:3}),
     body('email','Enter a valid Email').isEmail(),
     body('password','password must be min of 6 characters').isLength({min:6})
-],(req,res)=>{
+],async(req,res)=>{
     const errors= validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()});
     }
-    User.create({
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password,
-    }).then(user => res.json(user))
-    .catch(err=>{console.log(err)
-        res.json({error:'please enter Unique values',message:err.message})})
+    try{//finding if email already exist in database other wise store in db else throw error
+    let user=await User.findOne({email:req.body.email});
+        if(user)
+        return res.status(400).json({error:"User with this email Already exist!!!"});
+        //Salting and Hashing the password
+        const salt=await bcrypt.genSaltSync(10);
+        const secPswd=await bcrypt.hash(req.body.password,salt)
 
+        user=await User.create({
+            name:req.body.name,
+            email:req.body.email,
+            password:secPswd,
+        })
+    }
+    catch (err){
+        console.log(err.message);
+        res.status(500).send("Some Error Occured")
+    }
+    const JWT_Secret='Kashif$06';
+
+    data={
+        user:User.Id,
+    }
+    let authtoken=jwt.sign(data,JWT_Secret);
+
+    res.json({authtoken})
 })
 
 module.exports= router
